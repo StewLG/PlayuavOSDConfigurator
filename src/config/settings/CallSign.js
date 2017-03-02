a56d1740070810546576020f8ccfe54149edc32c
Stewart Loving-Gibbard <stew@lovinggibbard.com>
2017-03-01 22:12:06 -0800

    This isn't working any more than the last one, but it uses a new starting point/model for CallSign.js, so may work better, we'll soon see.

--- src/config/settings/CallSign.js
+++ src/config/settings/CallSign.js
@@ -1,64 +1,132 @@
 import React, { Component, PropTypes } from 'react';
-import ImmutablePropTypes from 'react-immutable-proptypes';
-import Parameters from '../parameters';
 import Input from '../../components/Input';
 import Column from '../../components/Column';
+import Select from '../parameters/Select';
+import SimpleSettings from './SimpleSettings';
+import ImmutablePropTypes from 'react-immutable-proptypes';
 import CustomPropTypes from '../../utils/PropTypes';
 
+// New: Derived from LinkQuality
 export default class CallSign extends Component {
   static propTypes = {
     parameters: ImmutablePropTypes.contains({
       fontSize: CustomPropTypes.value(PropTypes.number.isRequired).isRequired,
-      numberOfPanels: PropTypes.number.isRequired,      
+      numberOfPanels: PropTypes.number.isRequired,    
       positionX: CustomPropTypes.value(PropTypes.number.isRequired).isRequired,
       positionY: CustomPropTypes.value(PropTypes.number.isRequired).isRequired,
-      hAlignment: CustomPropTypes.value(PropTypes.number.isRequired).isRequired, 
-      //callSignText: CustomPropTypes.value(PropTypes.string.isRequired).isRequired, 
+      hAlignment: CustomPropTypes.value(PropTypes.number.isRequired).isRequired,
+      visibleOn: CustomPropTypes.value(PropTypes.number.isRequired).isRequired,
+      
+      //callSignText: CustomPropTypes.value(PropTypes.string.isRequired).isRequired,       
+          
     }).isRequired,
     setFontSize: PropTypes.func.isRequired,
-    setVisibleOn: PropTypes.func.isRequired,    
-    setPosition: PropTypes.func.isRequired,
-    setHAlignment: PropTypes.func.isRequired,   
-    //setCallSignText: PropTypes.func.isRequired,   
+    setVisibleOn: PropTypes.func.isRequired,
+    setPosition: PropTypes.func.isRequired,    
+    setHAlignment: PropTypes.func.isRequired,
+    // setMax: PropTypes.func.isRequired,
+    // setMin: PropTypes.func.isRequired,
+    // setRaw: PropTypes.func.isRequired,
+    // setType: PropTypes.func.isRequired,
   }
 
   shouldComponentUpdate(nextProps) {
     return !this.props.parameters.equals(nextProps.parameters);
   }
 
+  /*
+  _setMin = (min) => {
+    this.props.setMin('linkQuality', parseInt(min, 10));
+  }
+
+  _setMax = (max) => {
+    this.props.setMax('linkQuality', parseInt(max, 10));
+  }
+
+  _setType = (type) => {
+    this.props.setType('linkQuality', type);
+  }
+
+  _setRaw = (raw) => {
+    this.props.setRaw('linkQuality', raw);
+  }
+  */
+  
+  _setCallSignText = (raw) => {
+    //this.props.setRaw('callSign', raw);
+  }  
+  
+
   render() {
-    const { setFontSize, setPosition, setHAlignment,/* setCallSignText, */setVisibleOn } = this.props; 
-    const {
-      fontSize, numberOfPanels, positionX, positionY, hAlignment, /*callSignText,*/ visibleOn, 
-    } = this.props.parameters;
+    const { callSignText } = this.props.parameters;
+    /*
+    const rawOptions = [
+      { value: 0, label: 'percentage' }, { value: 1, label: 'raw value' }
+    ];
+    const typeOptions = [
+      { value: 5, label: 'rc 5' }, { value: 6, label: 'rc 6' },
+      { value: 7, label: 'rc 7' }, { value: 8, label: 'rc 8' },
+      { value: 9, label: 'rc 9' }, { value: 10, label: 'rc 10' },
+      { value: 11, label: 'rc 11' }, { value: 12, label: 'rc 12' },
+      { value: 13, label: 'rc 13' }, { value: 14, label: 'rc 14' },
+      { value: 15, label: 'rc 15' }, { value: 16, label: 'rc 16' }
+    ];
+    const minInput = 1000;
+    const maxInput = 2000;
+    */
     
     return (
-      <Parameters.ParameterList name="call sign">
-        <Parameters.Position labelX="position x" labelY="position y" name="callSign"
-          positionX={positionX} positionY={positionY} setPosition={setPosition}
-        />
-        <Column width={50} >
-          <Parameters.FontSize fontSize={fontSize} name="callSign" setFontSize={setFontSize} />
-        </Column>
-        <Column width={50} >
-          <Parameters.HorizontalAlignment name="callSign"
-            hAlignment={hAlignment} setHAlignment={setHAlignment}
-          />
-        </Column>
-        
+      <SimpleSettings label="call sign" name="callSign" {...this.props}>
+           
 
-        
-        <Parameters.VisibleOn visibleOn={visibleOn} name="callSign"
-          setVisibleOn={setVisibleOn} numberOfPanels={numberOfPanels}
-        />        
-                     
-      </Parameters.ParameterList>
-    );
+
+      </SimpleSettings>
+     );
   }
 }
 
-        /*
-        <Parameters.CallSignText  name="callSign"
-          callSignText={callSignText} setCallSignText={setCallSignText}
-        />   
-        */
+/*
+// Trying to get this column online:
+
+        <Column width={100} >
+          <Input type="string" 
+            label="Call Sign Text" value={callSignText} onChange={this._setCallSignText}
+          />
+        </Column>
+
+
+*/
+
+
+// Prior column setup
+/*
+
+      <SimpleSettings label="call sign" name="callSign" {...this.props}>
+        <Column width={50} >
+          <Input type="number" min={minInput} max={maxInput}
+            label="min" value={min} onChange={this._setMin}
+          />
+        </Column>
+        <Column width={50} >
+          <Input type="number" min={minInput} max={maxInput}
+            label="max" value={max} onChange={this._setMax}
+          />
+        </Column>
+        <Column width={50} >
+          <Select label="input type" value={type} options={typeOptions} setValue={this._setType} />
+        </Column>
+        <Column width={50} >
+          <Select label="display" value={raw} options={rawOptions} setValue={this._setRaw} />
+        </Column>
+      </SimpleSettings>
+*/
+
+
+
+
+
+
+
+
+
+
