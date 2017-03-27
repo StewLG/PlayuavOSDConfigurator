c561fc84fdb3f3ea7a6d510378a3c316457519a7
Stewart Loving-Gibbard <stew@lovinggibbard.com>
2017-03-26 17:21:37 -0700

    This is a working summary preview I think. Problems: - Screwy z-order problems with other panels? - Wish it knocked out clear rather than black

--- src/preview/Summary.js
+++ src/preview/Summary.js
@@ -7,29 +7,39 @@ import fonts from '../utils/fonts';
 export default class Summary extends PreviewBase {
   static propTypes = {
     panel: PropTypes.number.isRequired,
-    positionX: PropTypes.number.isRequired,
-    positionY: PropTypes.number.isRequired,
+    //positionX: PropTypes.number.isRequired,
+    //positionY: PropTypes.number.isRequired,
     visibleOn: PropTypes.number.isRequired,
   }
 
   draw() {
     const font = fonts.getFont(0);
     this.canvas.clear();
+    const width = this.refs.canvas.width;
     const height = this.refs.canvas.height;
 
     if ((this.props.visibleOn & Math.pow(2, this.props.panel)) !== 0) {
+
+        // Draw rectangle that encloses whole summary panel        
+        this.canvas.drawFilledRectangle(0, 0, width, height, true, false);
+        this.canvas.drawRectangle(0, 0, width-1, height-1, true, true);
+
+        //this.canvas.clearRect(0, 0, width, height);
+
         var debugLines = [ 
-                            'abs h.b. 68',
-                            'rel h.b. -7',
-                            'comp. b 75'
+                            'Summary Value Line 1: ',
+                            'Summary Value Line 2: ',
+                            'Summary Value Line 3: '
                          ];    
-        var currentLineYOffset = 0;
+        var currentLineXOffset = 5;                         
+        var currentLineYOffset = 5;
+
         for (let debugLineIndex = 0; debugLineIndex < debugLines.length; debugLineIndex++) {
             // Get the current line
             var currentDebugLine = debugLines[debugLineIndex];
             // Draw the current line
-            var debugLineStringPosition = Canvas.calculateStringPosition(currentDebugLine, 0, currentLineYOffset, Canvas_types.H_ALIGNMENT_LEFT, Canvas_types.V_ALIGNMENT_TOP, font);
-            this.canvas.drawString(currentDebugLine, 0, currentLineYOffset, font); 
+            var debugLineStringPosition = Canvas.calculateStringPosition(currentDebugLine, currentLineXOffset, currentLineYOffset, Canvas_types.H_ALIGNMENT_LEFT, Canvas_types.V_ALIGNMENT_TOP, font);
+            this.canvas.drawString(currentDebugLine, currentLineXOffset, currentLineYOffset, font); 
             // Move Y position down one line
             currentLineYOffset += debugLineStringPosition.height;
         }
@@ -37,9 +47,13 @@ export default class Summary extends PreviewBase {
   }
 
   render() {
-    const { positionX, positionY } = this.props;
-    const height = 90;
-    const width = 300;
+    //const { positionX, positionY } = this.props;
+    // Position is hard-coded, as it is in the firmware. (TODO: Remove configurable position)
+    const positionX = 80;
+    const positionY = 40;
+    const width = 200;    
+    const height = 190;
+
     const visible = (this.props.visibleOn & Math.pow(2, this.props.panel)) !== 0;
 
     return (
@@ -48,8 +62,8 @@ export default class Summary extends PreviewBase {
         <canvas
           ref="canvas"
           style={{ left: positionX, top: positionY }}
-          width={height}
-          height={width}
+          width={width}
+          height={height}
           className="preview-widget"
         />
     );
--- src/utils/eeprom.js
+++ src/utils/eeprom.js
@@ -25,7 +25,7 @@ const defaultEEPROM = [
   // Parameters for home direction debug info panelChannel
   0, 0, 65, 70,
   // Parameters for Summary -- TOTALLY FAKE
-  0, 0, 65, 70, 8, 1000, 2000, 0, 0
+  0, 0, 65, 70, 8, 0, 1000, 2000, 0
 ];
 
 function toEnabled(byte) {
